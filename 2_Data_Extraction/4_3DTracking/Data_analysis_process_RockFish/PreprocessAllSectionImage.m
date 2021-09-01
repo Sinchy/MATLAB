d72a1035b45b37e0d0a6c7b2bcd0375cc03e98f4
ShiyongTan <tan_shiyong@126.com>
2021-09-01 18:34:12 -0400

    Change process directory

--- 2_Data_Extraction/4_3DTracking/Data_analysis_process_RockFish/PreprocessAllSectionImage.m
+++ 2_Data_Extraction/4_3DTracking/Data_analysis_process_RockFish/PreprocessAllSectionImage.m
@@ -7,7 +7,7 @@ for i = 1 : size(dfolders, 1)
     dir_process = PreprocessImage([project_path  '/' dfolders(i).name], calibration_file);
     GenerateJobConfiguration([dir_process]);
     if i > 1
-        copyfile([project_path  'S01/' calibration_name '.txt'], [dir_process]); % for section number smaller than 99
+        copyfile([project_path  '/S01/' calibration_name '.txt'], [dir_process]); % for section number smaller than 99
     end
 end
 end
